1fb70948b87a0b76706dc0cb79c939caed661e08
RaviSankarRao <ravisankar139@gmail.com>
2017-06-09 15:50:18 -0700

    changes for ingredients

---
+++ cook-book/src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient{
+    
+    // public name : string;
+    // public amount : number;
+
+    // constructor(name:string,amount:string){
+
+    // }
+
+    // OR
+
+    constructor(public name:string, public amount:number){
+
+    }
+}
