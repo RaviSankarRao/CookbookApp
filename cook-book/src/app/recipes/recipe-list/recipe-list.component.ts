7c3bcb3127350c9c7c063a6b0ec9e54076e303ee
RaviSankarRao <ravisankar139@gmail.com>
2017-06-09 15:16:45 -0700

    checkin in changes

--- cook-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cook-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes : Recipe[] = [
+    new Recipe('Recipe 1', 'description for the recipe', 'http://www.hurontitle.com/recipecard1.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
