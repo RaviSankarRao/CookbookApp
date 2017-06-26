8bb25d659ae5255e28f503f3e233dd354715a530
RaviSankarRao <ravisankar139@gmail.com>
2017-06-26 10:56:55 -0700

    added navigation using ngIf

--- cook-book/src/app/app.component.ts
+++ cook-book/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayRecipe:boolean;
+
+  onSelection(selection){
+    if(selection === 'recipe'){
+      this.displayRecipe = true;
+    }
+    else{
+      this.displayRecipe = false;
+    }
+  }
   
 }
